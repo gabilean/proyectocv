3cddeaf1ddae21c8d33d05127943f69357d5ef8b
Gabilean <gabixlean@gmail.com>
2023-01-30 17:54:23 -0300

    Add scripts and tailwindconfig for intellisense

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleText() {
+    var msg = document.getElementById("showMore");
+    if (msg.style.display === "none") {
+        msg.style.display = "block";
+    } else {
+        msg.style.display = "none";
+    }
+  }
